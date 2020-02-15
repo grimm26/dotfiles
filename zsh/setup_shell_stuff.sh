e6b1cacb3e90f5507573d18fd8e707d391500373
Mark Keisler <mark@mitsein.net>
2020-02-15 15:59:43 -0600

    improve shell setup

--- zsh/setup_shell_stuff.sh
+++ zsh/setup_shell_stuff.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 #
-set -x
+## This assumes Ubuntu.  Should be smarter.
+#set -x
 
 echo $PATH
 if whence -p brew ; then
@@ -9,6 +10,11 @@ if whence -p brew ; then
 else
   echo $?
   echo "Installing homebrew"
-  exit
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
+brew install starship
+brew install keychain
+brew install hub
+# zplug will not work correctly without GNU awk.
+sudo apt install gawk
