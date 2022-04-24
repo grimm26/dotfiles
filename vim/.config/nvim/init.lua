6503e85f72d4e488bee2b67f037180f450b583cd
Mark Keisler <mark@mitsein.net>
2022-04-23 20:02:51 -0500

    format rest of lua files

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -7,8 +7,8 @@ local set = vim.opt -- to set options
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(vim.fn.glob(install_path)) > 0 then
-    fn.execute("!git clone https://github.com/wbthomason/packer.nvim " ..
-                   install_path)
+  fn.execute("!git clone https://github.com/wbthomason/packer.nvim " ..
+  install_path)
 end
 -- Remap space as leader key
 -- vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -3,71 +3,71 @@
 -- vim.cmd [[packadd packer.nvim]]
 local packer_group = vim.api.nvim_create_augroup('Packer', {clear = true})
 vim.api.nvim_create_autocmd('BufWritePost', {
-    command = 'source <afile> | PackerCompile',
-    group = packer_group,
-    pattern = 'plugins.lua'
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = 'plugins.lua'
 })
 
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {{"nvim-lua/plenary.nvim"}}
-    })
-    use({
-        "kyazdani42/nvim-tree.lua",
-        requires = {{"kyazdani42/nvim-web-devicons"}} -- for file icons
-    })
-    use({
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            })
-        end
-    })
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = {{"nvim-lua/plenary.nvim"}}
+  })
+  use({
+    "kyazdani42/nvim-tree.lua",
+    requires = {{"kyazdani42/nvim-web-devicons"}} -- for file icons
+  })
+  use({
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end
+  })
 
-    -- Simple plugins can be specified as strings in a list
-    use({
-        "neovim/nvim-lspconfig",
-        'williamboman/nvim-lsp-installer',
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
-        "kamykn/popup-menu.nvim",
-        "averms/black-nvim",
-        "ellisonleao/glow.nvim",
-        "lewis6991/gitsigns.nvim",
-        "will133/vim-dirdiff",
-        "tpope/vim-endwise",
-        "tpope/vim-fugitive",
-        "fatih/vim-go",
-        "elzr/vim-json",
-        "tmux-plugins/vim-tmux",
-        "z0mbix/vim-shfmt",
-        "prettier/vim-prettier",
-        "kamykn/spelunker.vim",
-        "mrjones2014/legendary.nvim",-- keymappings, commands, autocmds
-        'stevearc/dressing.nvim', -- fancy ui menu with legendary
-    })
+  -- Simple plugins can be specified as strings in a list
+  use({
+    "neovim/nvim-lspconfig",
+    'williamboman/nvim-lsp-installer',
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "kamykn/popup-menu.nvim",
+    "averms/black-nvim",
+    "ellisonleao/glow.nvim",
+    "lewis6991/gitsigns.nvim",
+    "will133/vim-dirdiff",
+    "tpope/vim-endwise",
+    "tpope/vim-fugitive",
+    "fatih/vim-go",
+    "elzr/vim-json",
+    "tmux-plugins/vim-tmux",
+    "z0mbix/vim-shfmt",
+    "prettier/vim-prettier",
+    "kamykn/spelunker.vim",
+    "mrjones2014/legendary.nvim", -- keymappings, commands, autocmds
+    'stevearc/dressing.nvim', -- fancy ui menu with legendary
+  })
 
-    use({
-        "nvim-lualine/lualine.nvim",
-        requires = {"kyazdani42/nvim-web-devicons", opt = true}
-    })
-    -- colorschemes
-    use({"marko-cerovac/material.nvim", "ishan9299/nvim-solarized-lua"})
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = {"kyazdani42/nvim-web-devicons", opt = true}
+  })
+  -- colorschemes
+  use({"marko-cerovac/material.nvim", "ishan9299/nvim-solarized-lua"})
 
-    -- Post-install/update hook with neovim command
-    use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
-    use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-    use("nvim-treesitter/nvim-treesitter-textobjects")
+  -- Post-install/update hook with neovim command
+  use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
+  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+  use("nvim-treesitter/nvim-treesitter-textobjects")
 
-    use({"echasnovski/mini.nvim", branch = "stable"})
+  use({"echasnovski/mini.nvim", branch = "stable"})
 end)
