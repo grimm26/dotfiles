ce9659c46c82cf8dd91a4004829b52e9f0f3472f
Mark Keisler <mkeisler@enova.com>
2022-05-04 13:02:46 -0500

    disable current_line_blame in gitsigns nvim, add kitty open-action for text files

--- .config/nvim/lua/config_plugins.lua
+++ .config/nvim/lua/config_plugins.lua
@@ -19,7 +19,7 @@ require("gitsigns").setup({
   diff_opts = {
     internal = true
   },
-  current_line_blame = true,
+  current_line_blame = false,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
