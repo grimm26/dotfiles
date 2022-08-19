8d4a033360376dc18609555a13de82bd9c9a3f77
Mark Keisler <mark@mitsein.net>
2022-08-19 11:20:15 -0500

    Just removing all cmp/snip stuff until I know how to use it :)

--- .config/nvim/lua/config_plugins.lua
+++ .config/nvim/lua/config_plugins.lua
@@ -520,51 +520,58 @@ require("formatter").setup {
 }
 
 require "fidget".setup {}
---
--- The below cmp/snip was just copied from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
--- luasnip setup
-local luasnip = require("luasnip")
---
--- nvim-cmp setup
-local cmp = require("cmp")
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  }),
-  sources = {
-    {name = 'nvim_lsp'},
-    {name = 'luasnip'},
-  },
-}
+
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
+-- --
+-- -- The below cmp/snip was just copied from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+-- -- luasnip setup
+-- local luasnip = require("luasnip")
+-- --
+-- -- nvim-cmp setup
+-- local cmp = require("cmp")
+-- cmp.setup {
+--   snippet = {
+--     expand = function(args)
+--       luasnip.lsp_expand(args.body)
+--     end,
+--   },
+--   mapping = cmp.mapping.preset.insert({
+--     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--     ['<CR>'] = cmp.mapping.confirm {
+--       behavior = cmp.ConfirmBehavior.Replace,
+--       select = true,
+--     },
+--     ['<Tab>'] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_next_item()
+--       elseif luasnip.expand_or_jumpable() then
+--         luasnip.expand_or_jump()
+--       elseif has_words_before() then
+--         cmp.complete()
+--       else
+--         fallback()
+--       end
+--     end, {'i', 's'}),
+--     ['<S-Tab>'] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_prev_item()
+--       elseif luasnip.jumpable(-1) then
+--         luasnip.jump(-1)
+--       else
+--         fallback()
+--       end
+--     end, {'i', 's'}),
+--   }),
+--   sources = {
+--     {name = 'nvim_lsp'},
+--     {name = 'luasnip'},
+--   },
+-- }
 
 require("octo").setup({
   github_hostname = vim.env.GH_HOST; -- GitHub Enterprise host (if set)
