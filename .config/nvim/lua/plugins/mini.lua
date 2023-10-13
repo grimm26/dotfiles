560d16e79a765f12b898a28e6372b37a09b2d2bd
Mark Keisler <mark@mitsein.net>
2023-10-13 17:07:17 -0500

    nvim: move from mini.starter to dashboard-nvim.

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,16 +4,6 @@ return {
     version = false,
     config = function()
       -- Enable modules out of mini,nvim that we want to use
-      local starter = require("mini.starter")
-      starter.setup({
-        items = {
-          starter.sections.telescope(),
-        },
-        content_hooks = {
-          starter.gen_hook.adding_bullet(),
-          starter.gen_hook.aligning("center", "center"),
-        },
-      })
       require("mini.align").setup()
       require("mini.comment").setup({})
       -- require("mini.indentscope").setup({})
