0f28703babce48cab3903eda859a2e64b587b981
Mark Keisler <mark@mitsein.net>
2023-11-06 11:50:41 -0600

    nvim: add mikesmithgh/kitty-scrollback.nvim but in disabled state because it needs neovim >= 0.10

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -3,4 +3,16 @@ return {
   { "nvim-lua/plenary.nvim", lazy = true },
   { "onsails/lspkind.nvim", lazy = true },
   { "nvim-tree/nvim-web-devicons", lazy = true },
+  {
+    "mikesmithgh/kitty-scrollback.nvim",
+    enabled = false,
+    lazy = true,
+    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+    event = { "User KittyScrollbackLaunch" },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    version = "^2.0.0", -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require("kitty-scrollback").setup()
+    end,
+  },
 }
