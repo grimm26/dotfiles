efffb4483a1a45dd16f9e7f8702a23ba388f27fd
Mark Keisler <mark@mitsein.net>
2023-12-05 10:54:53 -0600

    nvim: toggleterm set insert_mappings to false

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -5,6 +5,7 @@ return {
       version = "*",
       opts = {
         open_mapping = "<leader>tt",
+        insert_mappings = false,
       },
       config = function(_, opts)
         require("toggleterm").setup(opts)
