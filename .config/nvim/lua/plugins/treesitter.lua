bb5522a8136854e9358a6c678a3707d0b20610fc
Mark Keisler <mark@mitsein.net>
2024-01-19 15:58:58 -0600

    Pin nvim-treesitter to v0.9.2 for now
    https://github.com/nvim-treesitter/nvim-treesitter/issues/2293#issuecomment-1900679583

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    tag = "v0.9.2",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSUpdateSync" },
