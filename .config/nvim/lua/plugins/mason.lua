c592784583b84bc205d036de1e3b1447acf77bf9
Mark Keisler <mark@mitsein.net>
2024-02-01 09:25:35 -0600

    nvim: Put the Mason autocmds back, stop printing empty table.

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -37,6 +37,7 @@ return {
           "marksman",
           "mdformat",
           "mdslw",
+          "misspell",
           "prettierd",
           "python-lsp-server",
           "stylua",
@@ -71,22 +72,26 @@ return {
         -- debounce_hours = 5, -- at least 5 hours between attempts to install/update
       })
       vim.api.nvim_create_user_command("MTU", "MasonToolsUpdate", { desc = "Run MasonToolsUpdate" })
-      -- vim.api.nvim_create_autocmd("User", {
-      --   pattern = "MasonToolsStartingInstall",
-      --   callback = function()
-      --     vim.schedule(function()
-      --       print("mason-tool-installer is starting")
-      --     end)
-      --   end,
-      -- })
-      -- vim.api.nvim_create_autocmd("User", {
-      --   pattern = "MasonToolsUpdateCompleted",
-      --   callback = function(e)
-      --     vim.schedule(function()
-      --       print(vim.inspect(e.data)) -- print the table that lists the programs that were installed
-      --     end)
-      --   end,
-      -- })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MasonToolsStartingInstall",
+        callback = function()
+          vim.schedule(function()
+            print("mason-tool-installer is starting")
+          end)
+        end,
+      })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MasonToolsUpdateCompleted",
+        callback = function(e)
+          if #e.data == 0 then
+            return
+          else
+            vim.schedule(function()
+              print("Mason packages installed/updated: ", vim.inspect(e.data)) -- print the table that lists the programs that were installed
+            end)
+          end
+        end,
+      })
     end,
   },
 }
