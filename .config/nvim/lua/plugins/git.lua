d73cc62171c97a2842f3723e3235de73001c9454
Mark Keisler <mark@mitsein.net>
2024-09-05 10:33:14 -0500

    nvim: merge git related plugins into one file. Add diffview

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -68,4 +68,29 @@ return {
       })
     end,
   },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        github_hostname = vim.env.GH_HOST, -- GitHub Enterprise host (if set)
+      })
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = "Git",
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
 }
