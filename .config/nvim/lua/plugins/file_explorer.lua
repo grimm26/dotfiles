21a3721c2605baef29dd7b08c1dca64fe6596007
Mark Keisler <mark@mitsein.net>
2025-06-01 16:08:44 -0500

    nvim: add yazi, disable oil

--- .config/nvim/lua/plugins/file_explorer.lua
+++ .config/nvim/lua/plugins/file_explorer.lua
@@ -2,6 +2,7 @@ return {
   {
     -- file explorer
     "stevearc/oil.nvim",
+    enabled = false,
     config = function()
       -- helper function to parse output
       local function parse_output(proc)
@@ -75,6 +76,49 @@ return {
       wk.add({
         { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
       })
+    end,
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- check the installation instructions at
+      -- https://github.com/folke/snacks.nvim
+      "folke/snacks.nvim",
+    },
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      -- vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
     end,
   },
 }
