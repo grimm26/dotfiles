9a2f32bdf795cb7906bdfcd48f8f158c0fbd69ab
Mark Keisler <mark@mitsein.net>
2025-06-02 15:53:40 -0500

    yazi/nvim: set "-" keymap to go up a dir

--- .config/nvim/lua/plugins/file_explorer.lua
+++ .config/nvim/lua/plugins/file_explorer.lua
@@ -96,7 +96,7 @@ return {
       },
       {
         -- Open in the current working directory
-        "<leader>cw",
+        "-",
         "<cmd>Yazi cwd<cr>",
         desc = "Open the file manager in nvim's working directory",
       },
